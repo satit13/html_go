4b9bdcc3730698dd2f35195e791060c3a7517544
satit <satit@nopadol.com>
2017-12-25 15:32:53 +0700

    update code

--- mail/main.go
+++ mail/main.go
@@ -8,17 +8,27 @@ import (
 	"github.com/scorredoira/email"
 	"log"
 	_ "net/mail"
+	//"github.com/matryer/m"
 )
 
 func main() {
-	m := email.NewMessage("Hi", "this is the body")
+	//m := email.NewMessage("Hi", "this is the body")
+	m := email.NewHTMLMessage("Hi", "this is the body")
 	m.From.Name = "go@nopadol.com"
 	m.From.Address = "go mail"
-	m.To = []string{"satit@extensionsoft.biz"}
+	//m.To = []string{"tom@nopadol.com","satit@nopadol.com"}
+	m.To = []string{"satit@nopadol.com"}
+	m.Cc = []string{"it@nopadol.com"}
 	m.Body = "test send main from golang"
-	m.Subject = "send main from golang"
+	m.Subject = "send html mail from golang programming"
 
-	err := m.Attach("gofer.png")
+	err := m.Attach("./mail/gofer.png")
+	err = m.Attach("1.html")
+	// use Inline to display the attachment inline.
+	if err := m.Inline("main.go"); err != nil {
+		log.Fatal(err)
+	}
+
 	if err != nil {
 		log.Println(err)
 	}
